6721b8824adc3f43c3c883104d71b28f0bbd7a61
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-03-13 09:38:05 +0100

    Step 3 initialisation

---
+++ SpaceInvadersFramework/SpaceInvadersFramework/Invader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvadersFramework
+{
+    class Invader : SpriteGameObject
+    {
+        public Invader(string assetname, int layer = 0, string id = "", int sheetIndex = 0) : base(assetname, layer, id, sheetIndex)
+        {
+        }
+    }
+}
